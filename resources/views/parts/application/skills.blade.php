ff4211b72dbe4cf9fdd7f6b5d2281fb3849441dd
Rangvaldr <rangvaldr@protonmail.com>
2019-04-28 15:33:48 -0400

    Increase spacing on skills page

--- resources/views/parts/application/skills.blade.php
+++ resources/views/parts/application/skills.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-12 col-lg-7 card-columns">
+    <div class="col-12 col-lg-9 card-columns">
         @foreach($skills as $category => $skill)
         @php($escaped_cat = str_replace(' ', '-', $category))
         <div class="accordion" id="{{ $escaped_cat }}">
