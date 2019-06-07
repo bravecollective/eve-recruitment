b983b2984282ee741b5f10c3f22b5e4cea6edc78
Rangvaldr <rangvaldr@protonmail.com>
2019-06-07 19:51:45 -0400

    Fix responsiveness on pages with ad listing cards
    Closes #39

--- resources/views/available_ads.blade.php
+++ resources/views/available_ads.blade.php
@@ -1,11 +1,15 @@
 @extends('default')
 @section('content')
     <h1>Available Ads</h1>
-    <div class="card" style="max-width: 25%;">
-        <ul class="bg-dark list-group list-group-flush">
-        @foreach($ads as $ad)
-            <a class="text-white" href="/{{ $ad->slug }}" target="_blank"><li class="bg-dark list-group-item">{{ $ad->group_name }}</li></a>
-        @endforeach
-        </ul>
+    <div class="row">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card">
+                <ul class="bg-dark list-group list-group-flush">
+                @foreach($ads as $ad)
+                    <a class="text-white" href="/{{ $ad->slug }}" target="_blank"><li class="bg-dark list-group-item">{{ $ad->group_name }}</li></a>
+                @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/group_ads.blade.php
+++ resources/views/group_ads.blade.php
@@ -1,16 +1,20 @@
 @extends('default')
 @section('content')
     <h1>My Group Ads</h1>
-    <div class="card" style="max-width: 25%;">
-        <ul class="bg-dark list-group list-group-flush">
-        @foreach($ads as $ad)
-            @if(isset($permissions))
-             <a class="text-white" href="/group/ad/{{ $ad->id }}/permissions"><li class="bg-dark list-group-item">{{ $ad->group_name }}</li></a>
-            @else
-            <a class="text-white" href="/group/ad/{{ $ad->id }}"><li class="bg-dark list-group-item">{{ $ad->group_name }}</li></a>
-            @endif
-        @endforeach
-        </ul>
+    <div class="row">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card">
+                <ul class="bg-dark list-group list-group-flush">
+            @foreach($ads as $ad)
+                @if(isset($permissions))
+                    <a class="text-white" href="/group/ad/{{ $ad->id }}/permissions"><li class="bg-dark list-group-item">{{ $ad->group_name }}</li></a>
+                @else
+                    <a class="text-white" href="/group/ad/{{ $ad->id }}"><li class="bg-dark list-group-item">{{ $ad->group_name }}</li></a>
+                @endif
+            @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 @role('group admin')
     <a href="/group/ad/create"><button type="button" class="btn btn-primary">Create Ad</button></a>
