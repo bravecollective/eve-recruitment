b6396a7e736a972089d4b66bcbcacc9fd325335e
Rangvaldr <rangvaldr@protonmail.com>
2020-08-29 12:25:24 -0400

    Show who set an app to awaiting information

--- resources/views/parts/applicant_row.blade.php
+++ resources/views/parts/applicant_row.blade.php
@@ -19,6 +19,11 @@
     <div class="col-xl-2 col-3">
         {{ $app->updated_at->format('Y-m-d H:i') }}
     </div>
+    <div class="col-xl-2 col-3">
+    @if($app->changelog()->orderBy('created_at', 'desc')->first()->new_state == \App\Models\Application::ON_HOLD)
+        Assigned recruiter: {{ \App\Models\Account::find($app->changelog()->orderBy('created_at', 'desc')->first()->account_id)->main()->name }}
+    @endif
+    </div>
     <div class="col-1">
         <a target="_blank" href="/application/{{ $app->id }}"><button type="button" class="btn btn-primary">Open</button></a>
     </div>
